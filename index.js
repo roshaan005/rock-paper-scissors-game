9edfaa37e920de39dcc3f8e33cc2ec973e95c1c7
anxious gritt <roshanjalil609@gmail.com>
2021-04-25 13:22:50 +0500

    changed the strings

--- index.js
+++ index.js
@@ -6,58 +6,58 @@ function computerPlay(){
   let randomValue = Math.floor(Math.random()*3)
   return options[randomValue]
 }
+
+// initialized points (0) for both the parties 
 let playerPoints = 0;
 let computerPoints = 0;
 
-// funtion for firstround of the game 
+// funtion for conditions in the game
 function playRound(computerSelection,playerSelection){
   if (playerSelection===computerSelection){
-    alert("n")
-    playerPoints+=1
-    computerPoints+=1
-    return "its a tie " 
+    alert("ummmm this is a tie , both the players chose the same weapon")
+    return "ummmm this is a tie , both the players chose the same weapon"
+  
   }
   else if (playerSelection==="rock" && computerSelection==="paper"){
     computerPoints+=1
-    alert("n")
-    return "paper loses to rock , the computer wins" 
+    alert("the computer chose paper , rock loses to paper , darnn you lost this round")
+    return "the computer chose paper , rock loses to paper , darnn you lost this round" 
     
   }
   else if (playerSelection==="rock" && computerSelection==="scissors"){
     playerPoints+=1
-    alert("n")
-    return "sci loses to rock" 
+    alert("the computer chose scissors , scissors lose to rock , hehe you won this round wohooo")
+    return "the computer chose scissors , scissors lose to rock , hehe you won this round wohooo" 
    
-  }
-  else if (playerSelection==="rock" && computerSelection==="paper"){
-    computerPoints+=1
-    alert("n")
-    return "rock loses to paper" 
   }
   else if (playerSelection==="paper" && computerSelection==="scissors"){
     computerPoints+=1
-    alert("n")
-    return "paper loses to sci" 
+    alert("the computer chose scissors, paper loses to scissors, darnn you lost this round")
+    return "the computer chose scissors, paper loses to scissors, darnn you lost this round" 
   }
   else if (playerSelection==="paper" && computerSelection==="rock"){
     playerPoints+=1
-    alert("n")
-    return "rock loses to paper" 
+    alert("the computer chose rock, rock loses to paper, hehe you won this round weeee")
+    return "the computer chose rock, rock loses to paper, hehe you won this round weeee" 
   }
   else if (playerSelection==="scissors" && computerSelection==="rock"){
     computerPoints+=1
-    alert("n")
-    return "sci lose to rock" 
+    alert("the computer chose rock, scissors lose to rock, darn you lost this round")
+    return "the computer chose rock, scissors lose to rock, darn you lost this round" 
   }
   else if (playerSelection==="scissors" && computerSelection==="paper"){
     playerPoints+=1
-    alert("n")
-    return "paper loses to sci " 
+    alert("the computer chose paper, paper loses to scissors, YOU WON IT MATE")
+    return "pthe computer chose paper, paper loses to scissors, YOU WON IT MATE" 
   }
   else{
     return "darn maybe you made a mistake with the spellings , try again"
   }
 }
+
+
+
+// function for the game play 
 function game(){
   let computerSelection = computerPlay()
   const playerChoice = prompt("choose one of the three : rock , paper or scissors")
@@ -65,34 +65,32 @@ function game(){
   console.log(playRound(computerSelection,playerSelection))
 }
 
-function winner(computerPoints,playerPoints){
-  if (computerPoints>playerPoints){
-    alert("computer won")
-  }
-  else if (playerPoints>computerPoints){
-    alert("player won")
-  }
-  else{
-    alert("its a tie")
-  }
-}
-
+// loop for five rounds nested in a while loop with the playagain option
 let play = true;
 while (play = true){
   for (i = 0; i < 5; i++){
     game();
   }
-  winner()
-  let playAgain= prompt("do u wanna play again Y/n");
-  if (playAgain=='y'){
+  // winner function 
+  winner(computerPoints,playerPoints)
+  let playAgain= prompt("do u wanna play again (yes or yes?) jk : y/n");
+  if (playAgain.toLowerCase=='y'){
     play = true;
   }
   else{
-    play = false;
+    break;
   }
-
-
+}
+// winner calculator function
+function winner(computerPoints,playerPoints){
+  if (computerPoints>playerPoints){
+    alert("the computer defeated you lill hooman bean")
+  }
+  else if (playerPoints>computerPoints){
+    alert("AND you have defeated the mighty computer , have some candy :3")
+  }
+  else if (playerPoints === computerPoints){
+    alert("both the players were too good so this match was tied , you can try again buddy to defeat the mighty computer")
+  }
 }
 
-
-  
